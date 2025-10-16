69d8ad3003dce928a5f56eedba7fb747e2d6c8e1
Isak Johansson Weckstén <ij222pv@student.lnu.se>
2025-10-17 00:28:23 +0200

    Add validation for checkboxes

--- src/ui/FormHandler.ts
+++ src/ui/FormHandler.ts
@@ -28,20 +28,50 @@ export default class FormHandler {
   }
 
   private attachEventListener(): void {
-    this.form.addEventListener("submit", (e) => {
-      e.preventDefault();
-      const formData = new FormData(this.form);
-      this.executeCallbacks(formData);
-    });
+    this.form.addEventListener("submit", this.handleSubmitEvent.bind(this));
+  }
+
+  private handleSubmitEvent(event: Event): void {
+    event.preventDefault();
+    if (!this.validateInput()) return;
+    this.executeCallbacks();
+  }
+
+  private validateInput(): boolean {
+    if (!this.areAnyCheckboxesChecked()) {
+      this.reportNoCheckedCheckboxes();
+      return false;
+    }
+    return true;
+  }
+
+  private areAnyCheckboxesChecked(): boolean {
+    const checkboxes = this.getAllCheckboxes();
+    return checkboxes.some((checkbox) => checkbox.checked);
+  }
+
+  private getAllCheckboxes(): HTMLInputElement[] {
+    return Array.from(
+      this.form.querySelectorAll("input[type=checkbox]")!,
+    ) as HTMLInputElement[];
+  }
+
+  private reportNoCheckedCheckboxes(): void {
+    const checkboxes = this.getAllCheckboxes();
+    checkboxes.at(-1)!.setCustomValidity("Please select at least one metric.");
+    this.form.reportValidity();
+    // If we don't reset the validity, the error message will show every time the
+    // form is submitted, bypassing the submit handler.
+    checkboxes.at(-1)!.setCustomValidity("");
   }
 
   private addCallback(callback: FormCallback): void {
     this.callbacks.push(callback);
   }
 
-  private executeCallbacks(formData: FormData): void {
+  private executeCallbacks(): void {
     for (const callback of this.callbacks) {
-      callback(formData);
+      callback(new FormData(this.form));
     }
   }
 }
